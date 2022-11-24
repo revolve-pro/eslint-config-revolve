d6eaca9032c3ef7b5bedcab9f17a7ac4b1064a3b
Jakub Gańczorz <jakub.ganczorz@revolve.pro>
2022-11-24 10:09:42 +0100

    RN changes

--- react-native.js
+++ react-native.js
@@ -10,5 +10,24 @@ module.exports = {
     },
   },
   rules: {
+    'no-console': 1,
+    'react-native/no-unused-styles': 1,
+    'react-native/split-platform-components': 2,
+    'react-native/no-inline-styles': 2,
+    'react-native/no-single-element-style-arrays': 2,
+    // 'react-native/no-raw-text': 2, https://github.com/Intellicode/eslint-plugin-react-native/issues/307
+    'react-native/no-color-literals': 1,
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      },
+    ],
+    'no-void': 0,
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      { checksVoidReturn: { attributes: false } },
+    ],
   },
 };
